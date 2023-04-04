1bc941ae40ad757891e6f678f7a02b877b049ec4
Scott Han <38560421+fkdlrhtm45@users.noreply.github.com>
2023-04-04 16:30:55 +0900

    Update prac.cpp

--- prac.cpp
+++ prac.cpp
@@ -1,50 +1,42 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include "Header.h"
 #include <cstring>
-using namespace std;
-// header file로 넘어가면 안써도된다.
+using string::std;
 
-// 사용자로부터 실수 입력받는 함수
-// 사용자로부터 문자를 입력받는 함수
-
-class Student {
+class VertexStruct {
+private:
+    float x, y, z;
 public:
-    int student_no;
-    char name[20];
-    int gender; // 0 = male, 1 = female
-    
-    void printStudentInfo() {
-        printf("student ID: %d\n", student_no);
-        printf("studnet name: %s\n", name);
-        printf("sex: %s", gender == 0 ? "male" : "female");
-    }
+    void setToZero();
+    void set(float vx, float vy, float vz);
+    void showSet();
 };
+// From class VertexStrcut to here, could be the header.h file of class VertexStrcut 
+void VertexStruct::setToZero() {
+    x = 0;
+    y = 0;
+    z = 0;
+}
+void VertexStruct::set(float vx, float vy, float vz) {
+    this->x = vx;
+    this->y = vy;
+    this->z = vz;
+}
+void VertexStruct::showSet() {
+    cout << "Vx: " << x << ", Vy: " << y << ", Vz: " << z << '\n';
+}
+// From void setToZero() to void showSet() you could put those at VertextStruct.cpp file 
+// If you are gonna use it all at one file, need to add VertexStruct::setToZero() like this.
 
-int main()
-{
-    Student s;
-    s.student_no = 1234;
-    strcpy(s.name, "Scott");
-    s.gender = 0;
 
-    s.printStudentInfo();
+int main(){
 
-    Student s2;
-    s2.student_no = 3456;
-    strcpy(s2.name, "Julia");
-    s2.gender = 1;
-
-    s2.printStudentInfo();
-
-
-
+    VertexStruct v;
+     /*v.x = 1;      // all of them are inaccessible, bc they are private
+    v.y = 2;
+    v.z = 3;*/
+    v.set(3, 5, 8);
+    v.showSet();
 
     return 0;
 }
-
-
-
-
-
-
-
